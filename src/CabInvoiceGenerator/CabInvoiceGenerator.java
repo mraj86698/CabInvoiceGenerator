e69537880e20b7ba287e016269b213c920f014f0
MohanRaj <mraj86698@gmail.com>
2022-08-29 17:38:16 +0530

    Added to Welcome Message

---
+++ src/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -0,0 +1,10 @@
+package CabInvoiceGenerator;
+
+public class CabInvoiceGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Cab Invoice Generator");
+
+	}
+
+}
