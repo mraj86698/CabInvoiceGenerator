f9519e090d6ef1489bac8a6cab6803aba34fe8d2
MohanRaj <mraj86698@gmail.com>
2022-08-29 17:45:15 +0530

    Calculate TotalFare

--- src/CabInvoiceGenerator/CabInvoiceGenerator.java
+++ src/CabInvoiceGenerator/CabInvoiceGenerator.java
@@ -2,9 +2,19 @@ package CabInvoiceGenerator;
 
 public class CabInvoiceGenerator {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to the Cab Invoice Generator");
-
+	private static final int COST_PER_TIME = 1;
+	private static final double COST_PER_KM = 10;
+	private static final double MINIMUM_FARE = 5;
+	/**
+	 * Calculate Fare
+	 * Given Distance and Time The Invoice Generator should return the total fare of the Journey
+	 * Cost 10 per Km + Rs 1 Per Minute	and Minimum Fare - Rs. 5
+	 * @param distance
+	 * @param time
+	 * @return total fare
+	 */
+	public double calculateFare(double distance, int time) {
+		return Math.max(MINIMUM_FARE,distance * COST_PER_KM + time * COST_PER_TIME);
 	}
 
 }
